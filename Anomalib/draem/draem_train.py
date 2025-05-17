6391b8c2371fc0313b11695693fa8b79e54bcacd
Doğukan Kıyıklık <152120211104@ogrenci.ogu.edu.tr>
2025-05-17 19:17:24 +0300

    Update draem_train.py

--- Anomalib/draem/draem_train.py
+++ Anomalib/draem/draem_train.py
@@ -29,7 +29,7 @@ datamodule = Folder(
 
 # --------------------- Eğitim Motoru ---------------------
 engine = Engine(
-    max_epochs=20,
+    max_epochs=35,
     accelerator="auto",
     devices=1,
     default_root_dir="/content/anomaly_detection/checkpoints",
